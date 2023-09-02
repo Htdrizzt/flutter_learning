f0cdc5a367a9819f2e7a9a62c44ab5a78904d77e
drizzt.dev <yaser.yalman@gmail.com>
2023-09-02 16:36:33 +0300

    third comit değişim yapıldıt

--- lib/101/stateless__learn.dart
+++ lib/101/stateless__learn.dart
@@ -5,6 +5,48 @@ class StateLessLearn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return   Scaffold(
+      appBar: AppBar(),
+      body :    Column(
+        children: [
+          
+          const TitleTextWidget(text:"ali",),
+          const TitleTextWidget(text: "veli",),
+          _emptyBox(),
+          const TitleTextWidget(text: "yaser",),
+          _emptyBox(),
+          const TitleTextWidget(text: "zehra",),
+
+          const _CustomContainer(),
+          _emptyBox()
+        ]
+      )
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  SizedBox _emptyBox() => const SizedBox(height: 20,); //basit şeyleri böyle yapabilirsin
+}
+
+class _CustomContainer extends StatelessWidget {
+  const _CustomContainer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20), color: Colors.red
+      ),
+    );
+  }
+}
+  
+class TitleTextWidget extends StatelessWidget {
+  const TitleTextWidget({super.key,required this.text});
+  final String text ; 
+  @override
+  Widget build(BuildContext context) {
+    return Text(text
+          ,style:Theme.of(context ).textTheme.headlineMedium);
   }
 }
