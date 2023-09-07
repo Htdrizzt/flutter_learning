7e256e9e00bfd7c7b71ad088605177be35cce4e9
drizzt_dev <yaser.yalman@gmail.com>
2023-09-07 08:59:22 +0300

    asdasd

--- lib/101/stateful_learn.dart
+++ lib/101/stateful_learn.dart
@@ -8,21 +8,51 @@ class StatefulLearn extends StatefulWidget {
 }
 
 class _StatefulLearnState extends State<StatefulLearn> {
+  int _counter = 0;
+
+  void updateState(bool isIncerement) {
+    setState(() {
+      _counter += isIncerement ? 1 : -1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       floatingActionButton: Row(
         mainAxisSize: MainAxisSize.min,
+        children: [_increment(), _deincrement()],
+      ),
+      body: Column(
         children: [
-          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FloatingActionButton(onPressed: () {}, child: const Icon(Icons.remove)),
-          )
+          Center(
+              child: Text(
+            _counter.toString(),
+            style: Theme.of(context).textTheme.headlineLarge?.copyWith(color: Colors.red),
+          )),
+          const Placeholder(color: Colors.green,)
         ],
       ),
-      body: const Center(child: Text('1')),
+    );
+  }
+
+  FloatingActionButton _increment() {
+    return FloatingActionButton(
+        onPressed: () {
+          updateState(true);
+        },
+        child: const Icon(Icons.add));
+  }
+
+  Padding _deincrement() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: FloatingActionButton(
+          onPressed: () {
+            updateState(false);
+          },
+          child: const Icon(Icons.remove)),
     );
   }
 }
