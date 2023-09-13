ca679303f475502c9e254ae51e6db0b579e0caad
drizzt_dev <yaser.yalman@gmail.com>
2023-09-13 10:04:55 +0300

    12

--- lib/101/demos/my_collection_demos.dart
+++ lib/101/demos/my_collection_demos.dart
@@ -14,47 +14,43 @@ class _MyCollectionDemoState extends State<MyCollectionDemo> {
   @override
   void initState() {
     super.initState();
-    _items = [
-      CollectionModel(imagePath: 'assets/png/image2.png', title: 'Crows1', price: 24),
-      CollectionModel(imagePath: 'assets/png/imageCrow.jpg', title: 'Crows2', price: 24),
-      CollectionModel(imagePath: 'assets/png/image3.png', title: 'Crows3', price: 24),
-      CollectionModel(imagePath: 'assets/png/imageCrow.jpg', title: 'Crows4', price: 24)
-    ];                                      
+    _items = CollectionItems().items;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        itemCount: _items.length,
-        itemBuilder: (context, index) {
-        return  Card(
-          margin: const EdgeInsets.only(bottom: 30),
-          child: SizedBox(
-            height:300,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  Expanded(child: Image.asset(_items[index].imagePath,fit: BoxFit.fill,),),
-                  Padding(
-                    padding: const EdgeInsets.only(top:8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                              
-                      Text(_items[index].title),
-                      Text('${_items[index].price} TL')
-                      ],),
-                  )
-                ],
+          padding: PaddingUtility().paddingHoriontal,
+          itemCount: _items.length,
+          itemBuilder: (context, index) {
+            return Card(
+              margin: PaddingUtility().paddingBottom,
+              child: SizedBox(
+                height: 300,
+                child: Padding(
+                  padding: PaddingUtility().paddingAll,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Image.asset(
+                          _items[index].imagePath,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      Padding(
+                        padding: PaddingUtility().paddingTop,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [Text(_items[index].title), Text('${_items[index].price} TL')],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
-            ),
-
-          ),
-        );
-      }),
+            );
+          }),
     );
   }
 }
@@ -65,4 +61,29 @@ class CollectionModel {
   final int price;
 
   CollectionModel({required this.imagePath, required this.title, required this.price});
+}
+
+class PaddingUtility {
+  final paddingAll = const EdgeInsets.all(20.0);
+  final paddingBottom = const EdgeInsets.only(bottom: 30);
+  final paddingTop = const EdgeInsets.only(top: 8.0);
+  final paddingHoriontal = const EdgeInsets.symmetric(horizontal: 20);
+}
+
+class CollectionItems {
+  late final List<CollectionModel> items;
+  CollectionItems() {
+    items = [
+      CollectionModel(imagePath: ProjectImageUrls.image1, title: 'Crows1', price: 24),
+      CollectionModel(imagePath: ProjectImageUrls.image2, title: 'Crows2', price: 24),
+      CollectionModel(imagePath: ProjectImageUrls.image3, title: 'Crows3', price: 24),
+      CollectionModel(imagePath: ProjectImageUrls.image2, title: 'Crows4', price: 24)
+    ];
+  }
+}
+
+class ProjectImageUrls {
+  static const image1 = 'assets/png/image2.png';
+  static const image2 = 'assets/png/imageCrow.jpg';
+  static const image3 = 'assets/png/image3.png';
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:learning_101/main.dart';
+import 'package:learning_101/101/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
